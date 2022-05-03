40b36bb76e2e52f023df645dcaa510488a221b10
Decoderseeman <99487702+Decoderseeman@users.noreply.github.com>
2022-05-03 08:45:08 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help ', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates 📢', url='https://t.me/josprojects/221')
+            InlineKeyboardButton('📢 Updates 📢', url='https://t.me/mkv_rockers')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
